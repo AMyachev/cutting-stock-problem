eafad87fc04cf023bda3371b8e956580f4cc5f39
Anatolii Myachev <anatoliimyachev@mail.ru>
2019-09-17 00:29:27 +0300

    not working now

--- heuristics/heuristics.go
+++ heuristics/heuristics.go
@@ -1,7 +1,6 @@
 package heuristics
 
 import (
-	"fmt"
 	taskPackage "csp/task"
 	solutionPackage "csp/solution"
 
@@ -74,24 +73,75 @@ func pairExchange(permutation *[]int, i int, j int) {
 
 
 func LocalOptimization(task taskPackage.Task, permutation []int) ([]int, solutionPackage.Solution) {
-	computeCriterion := func(task taskPackage.Task, permutation []int) (int, int) {
-		solution := GreedyAlgorithm(task, permutation)
+	computeCriterion := func(task *taskPackage.Task, permutation *[]int) (int, int) {
+		solution := GreedyAlgorithm(*task, *permutation)
 		return solution.GetCountUsedMaterials(), solution.GetAllFreeLength()
 	}
 
-	bestCriterion, bestAllFreeLength := computeCriterion(task, permutation)
+	bestCriterion, bestAllFreeLength := computeCriterion(&task, &permutation)
 
 	for idx := 0; idx < task.GetCountPieces() - 1; idx++ {
 		pairExchange(&permutation, idx, idx + 1)
-		tempCriterion, allFreeLength := computeCriterion(task, permutation)
+		tempCriterion, allFreeLength := computeCriterion(&task, &permutation)
 		if tempCriterion < bestCriterion || (tempCriterion == bestCriterion) && (allFreeLength < bestAllFreeLength) {
 			bestCriterion = tempCriterion
 			bestAllFreeLength = allFreeLength
-			fmt.Println("better")
 			continue
 		}
 		pairExchange(&permutation, idx, idx + 1)
 	}
 
 	return permutation, GreedyAlgorithm(task, permutation)
+}
+
+
+func firstNotAssigned(permutationIndexes []int) (int, int) {
+	for position, idx := range permutationIndexes {
+		if idx != -1 {
+			return position, idx
+		}
+	}
+}
+
+func assign(permutationIndexes []int, position int) int {
+	permutationIndexes[position] = -1
+}
+
+func Search(task taskPackage.Task) ([]int, solutionPackage.Solution) {
+	solution := solutionPackage.MakeEmptySolution(task.GetMaterialLength())
+	permutation := task.GetAllPiecesByProperty("descending")
+	assigned_pieces := 0
+	all_pieces := task.GetCountPieces()
+	materialLength := task.GetMaterialLength()
+
+
+	for countUsedMaterial := 0;; countUsedMaterial++ {
+		position, idxPiece := firstNotAssigned(permutation)
+		solution.CutDetailFromNewMaterial(idxPiece, task.GetPieceLength(idxPiece))
+		assign(permutation, position)
+		assigned_pieces++
+		if assigned_pieces == all_pieces {
+			break
+		}
+
+		materialFreeLength := solution.GetFreeLength(countUsedMaterial)
+		for position, idxPiece := range permutation {
+			if idxPiece == -1 {
+				idxPieceLength := task.GetPieceLength(idxPiece)
+				if idxPieceLength > materialFreeLength {
+					continue
+				} else if idxPieceLength == materialFreeLength {
+					solution.CutDetail(countUsedMaterial, idxPiece, task.GetPieceLength(idxPiece))
+					assign(permutation, position)
+					assigned_pieces++
+					break
+				}
+			}
+		}
+
+		if assigned_pieces == all_pieces {
+			break
+		}
+
+	}
 }
