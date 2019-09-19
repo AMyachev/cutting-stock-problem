ef80f77f3d6e30673e8692e569c6dc461ba66ea0
Anatolii Myachev <anatoliimyachev@mail.ru>
2019-09-20 00:26:41 +0300

    implemented computing deviation

--- cmd/compute.go
+++ cmd/compute.go
@@ -19,12 +19,14 @@ func init() {
 
 	rootCmd.AddCommand(computeCmd)
 	computeCmd.Flags().StringVar(&taskFile, "task-file", "", "path to task-file (required)")
-	computeCmd.Flags().StringVar(&algorithm, "algo", "greedy-for-ascending", "algorithm that would be used for computation")
+	computeCmd.Flags().StringVar(&algorithm, "algo", "greedy-for-ascending", "algorithm that would be used for computation: {greedy-for-ascending, greedy-for-descending, local-optim-descending, search}")
+	computeCmd.Flags().StringVar(&deviation, "deviation", "lower-bound", "the value relative to which the deviation will be calculated: {lower-bound, greedy-value}")
 }
 
 var taskDir string
 var taskFile string
 var algorithm string
+var deviation string
 
 var computeCmd = &cobra.Command{
 	Use:   "compute",
@@ -32,7 +34,7 @@ var computeCmd = &cobra.Command{
     Long:  `<add description>`,
     Run: func(cmd *cobra.Command, args []string) {
 		if taskFile != "" {
-			computeProblem(taskFile, algorithm)
+			computeProblem(taskFile, algorithm, deviation)
 		} else {
 			files, err := ioutil.ReadDir(taskDir)
 			if err != nil {
@@ -43,13 +45,13 @@ var computeCmd = &cobra.Command{
 			}
 		
 			for _, file := range files {
-				computeProblem(filepath.Join(taskDir, file.Name()), algorithm)
+				computeProblem(filepath.Join(taskDir, file.Name()), algorithm, deviation)
 			}
 		}
     },
 }
 
-func computeProblem(taskFile string, algorithm string) {
+func computeProblem(taskFile string, algorithm string, deviation string) {
 	var solution solutionP.Solution
 
 	task, err := taskP.MakeOneDimensionalCuttingStockProblemFromFile(taskFile)
@@ -85,5 +87,21 @@ func computeProblem(taskFile string, algorithm string) {
 	end := time.Now()
 	elapsed := end.Sub(start)
 
-	fmt.Printf("%s \t %s: %d; \t time: %v; \t lower bound: %d\n", taskFile, algorithm, solution.GetCountUsedMaterials(), elapsed, task.ComputeLowerBound())
+	criterion := solution.GetCountUsedMaterials()
+	var deviationValue float64
+
+	switch deviation {
+	case "lower-bound":
+		value := task.ComputeLowerBound()
+		deviationValue = float64(criterion - value) / float64(value)
+	case "greedy-value":
+		greedySolution := heuristics.GreedyAlgorithmForAscending(task)
+		value := greedySolution.GetCountUsedMaterials()
+		deviationValue =  float64(value - criterion) / float64(value)
+	default:
+		panic("not implemented")
+	}
+
+	fmt.Printf("%s \t %s: %d; \t deviation: %f; \t lower bound: %d; \t time: %v;\n", taskFile, algorithm,
+				solution.GetCountUsedMaterials(), deviationValue, task.ComputeLowerBound(), elapsed)
 }
