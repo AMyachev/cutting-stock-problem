1ce265171fae8dc0c0dc3fe8297de94e267f1d74
Anatolii Myachev <anatoliimyachev@mail.ru>
2019-09-30 00:46:48 +0300

    bnb is working now

---
+++ cmd/computeDP.go
@@ -0,0 +1,66 @@
+// compute delivery problem command
+package cmd
+
+import (
+	"fmt"
+	"time"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	log "github.com/sirupsen/logrus"
+
+	"csp/exact_algorithms"
+	"csp/task"
+)
+
+func init() {
+	deliveryProblemsDir = "example-tasks/delivery-problem"
+
+	rootCmd.AddCommand(computeDPCmd)
+
+	computeDPCmd.Flags().StringVar(&deliveryProblemFile, "task-file", "", "path to file with delivery problem")
+	computeDPCmd.Flags().StringVar(&branchingStrategy, "branching", "optimistic", "branching strategy for branch and bound method: {}")
+	computeDPCmd.Flags().StringVar(&upperBoundStrategy, "upper-bound", "default", "strategy of finding upper bounds: {}")
+	computeDPCmd.Flags().StringVar(&lowerBoundStrategy, "lower-bound", "default", "strategy of finding lower bounds: {}")
+}
+
+var deliveryProblemsDir string
+var deliveryProblemFile string
+
+var branchingStrategy string
+var upperBoundStrategy string
+var lowerBoundStrategy string
+
+var computeDPCmd = &cobra.Command{
+	Use:   "computeDP",
+    Short: "compute delivery problems",
+    Long:  `<add description>`,
+    Run: func(cmd *cobra.Command, args []string) {
+		if deliveryProblemFile != "" {
+			ComputeDPProblem(deliveryProblemFile, branchingStrategy, lowerBoundStrategy, upperBoundStrategy)
+		} else {
+			files, err := ioutil.ReadDir(taskDir)
+			if err != nil {
+				log.WithFields(log.Fields{
+					"taskDir": taskDir,
+				}).Fatalf("computeDPCmd: %s", err)
+			}
+		
+			for _, file := range files {
+				ComputeDPProblem(filepath.Join(deliveryProblemsDir, file.Name()), branchingStrategy, lowerBoundStrategy, upperBoundStrategy)
+			}
+		}
+    },
+}
+
+func ComputeDPProblem(taskFile, branchingStrategy, lowerBoundStrategy, upperBoundStrategy string) {
+	start := time.Now()
+	taskDP := task.MakeDeliveryProblemFromFile(taskFile, branchingStrategy, lowerBoundStrategy, upperBoundStrategy)
+	bestVertex, countTraversedVertexes := exact_algorithms.BnB(taskDP)
+	fmt.Println("best Vertex: ", bestVertex)
+	fmt.Println("countTraversedVertexes: ", countTraversedVertexes)
+	end := time.Now()
+	elapsed := end.Sub(start)
+	fmt.Printf("time: %v", elapsed)
+}
--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 import (
 	"os"
 
-	//"csp/cmd"
-	"csp/exact_algorithms"
+	"csp/cmd"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func init() {
 	log.SetOutput(os.Stdout)
-	log.SetLevel(log.WarnLevel)
+	log.SetLevel(log.InfoLevel)
 }
 
 func main() {
 	//cmd.Execute()
-	exact_algorithms.BnB()
+	cmd.ComputeDPProblem("example-tasks/delivery-problem/task_2_10_n50.txt", "optimistic", "default", "default")
 }
